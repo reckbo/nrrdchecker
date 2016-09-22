8b3fadd13aaf63514c2d9e30ed51da7dcacee845
reckbo <reckbo@bwh.harvard.edu>
2016-09-22 18:34:00 -0400

    STY: update test.sh with better output

--- test.sh
+++ test.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
-stack exec nrrdchecker -- -i test-data/mask.nrrd -r test-data/mask.nrrd
-stack exec nrrdchecker -- -i test-data/mask.nrrd -r test-data/ref.nhdr
-stack exec nrrdchecker -- -i test-data/dwi.nhdr -r test-data/dwi.nhdr
-stack exec nrrdchecker -- -i test-data/bad.nhdr -r test-data/ref.nhdr
+
+test() {
+    echo "# TEST =================================="
+    cmd=$1
+    resultExpect=$2
+    out=$($cmd) 
+    result=$(echo $out | cut -d"," -f2)
+    if [[ "$result" == "$resultExpect" ]]; then
+        echo "# PASS"
+    else
+        echo "# FAIL"
+    fi
+    echo "========================================="
+}
+
+test "nrrdchecker -i test-data/mask.nrrd -r test-data/mask.nrrd" "pass"
+test "nrrdchecker -i test-data/mask.nrrd -r test-data/ref.nhdr" "fail"
+test "nrrdchecker -i test-data/dwi.nhdr -r test-data/dwi.nhdr" "pass"
+test "nrrdchecker -i test-data/bad.nhdr -r test-data/ref.nhdr" "fail"
